7ab833f63272c9d4c507bcf23d4bff89ba2450fc
harshay <harshaykr@hotmail.com>
2022-05-02 21:18:46 +0100

    station stats description comment added

--- bikeshare_2_harshay_ndegree_udacity_pfds_3.py
+++ bikeshare_2_harshay_ndegree_udacity_pfds_3.py
@@ -103,7 +103,7 @@ def time_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-
+# function calculation status stats
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
