868c9af146cc79499e2bef5fba83d0e613bfc0a6
harshay <harshaykr@hotmail.com>
2022-05-02 19:31:55 +0100

    github link added in comments

--- bikeshare_2_harshay_ndegree_udacity_pfds_3.py
+++ bikeshare_2_harshay_ndegree_udacity_pfds_3.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+#https://github.com/harshay/pdsnd_github git hub link
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
